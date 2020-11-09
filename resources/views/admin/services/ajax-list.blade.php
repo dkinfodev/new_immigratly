f1356a0a6f4d6252aa5a31751e0a16662c2519d8
Dk Dev <dkdev2415@gmail.com>
2020-11-08 21:15:56 -0800

    changes done

--- resources/views/admin/services/ajax-list.blade.php
+++ resources/views/admin/services/ajax-list.blade.php
@@ -14,8 +14,32 @@
       @endif
   </td>
   <td>{{$record->price}}</td>
-  <td> <a href="{{baseUrl('services/edit/'.base64_encode($record->id))}}"><i class="tio-edit"></i></a> &nbsp; <a href="{{baseUrl('licence-bodies/delete/'.base64_encode($record->id))}}"><i class="tio-delete"></i></a> 
-  
-</td>
+  <td> 
+    <div class="hs-unfold">
+      <a class="js-hs-action btn btn-sm btn-white" href="javascript:;"
+         data-hs-unfold-options='{
+           "target": "#action-{{$key}}",
+           "type": "css-animation"
+         }'>
+              More <i class="tio-chevron-down ml-1"></i>
+      </a>
+
+      <div id="action-{{$key}}" class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-sm dropdown-menu-right">
+        <a class="dropdown-item" href="{{baseUrl('services/edit/'.base64_encode($record->id))}}">Edit</a>
+        <a class="dropdown-item" href="{{baseUrl('services/folders/'.base64_encode($record->id))}}">Document Folders</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item text-danger" href="{{baseUrl('services/delete/'.base64_encode($record->id))}}">Delete</a>
+      </div>
+    </div>
+  </td>
 </tr>
 @endforeach
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+  $('.js-hs-action').each(function () {
+    var unfold = new HSUnfold($(this)).init();
+  });
+})
+</script>
